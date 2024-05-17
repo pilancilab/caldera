b7ea4abe3ac405c8904899d5629c53aaaadc0d6e
Naomi Sagan <nsagan@stanford.edu>
2024-05-16 17:57:24 -0700

    Update FT wikitext and ppl eval

--- shell_scripts/run_finetune_wikitext.sh
+++ shell_scripts/run_finetune_wikitext.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-DEVICES="4,5,6,7"
-MODELS=( "lplr-ldlq-4B-factors-downdate" "lplr-ldlq-rank-128-4B-factors" "lplr-ldlq-rank-128-4B-factors-downdate" "lplr-ldlq-rank-256-4B-factors-downdate" )
+DEVICES="0,1,2,3"
+# MODELS=( "lplr-ldlq-rank-64-16B-factors-downdate" "lplr-ldlq-rank-64-4B-factors-downdate" "lplr-ldlq-rank-128-4B-factors-downdate" "lplr-ldlq-rank-256-4B-factors-downdate" )
+MODELS=( "lplr-ldlq-rank-256-4B-factors-downdate" "lplr-ldlq-rank-64-16B-factors-downdate" )
 
 for MODEL in "${MODELS[@]}"
 do
-    CUDA_VISIBLE_DEVICES=$DEVICES accelerate launch --config_file shell_scripts/accelerate_config.yaml \
+    CUDA_VISIBLE_DEVICES=$DEVICES accelerate launch --config_file shell_scripts/accelerate_config_2.yaml \
         scripts/finetune_wikitext.py \
-        --model_save_path /media/hdd1/lplr-q-models/llama-2-7b/$MODEL/ \
-        --output_dir /media/hdd1/lplr-q-finetune/llama-2-7b/wikitext/$MODEL/ \
+        --model_save_path /media/hdd1/lplr-q-models/llama-3-8b/$MODEL/ \
+        --output_dir /media/hdd1/lplr-q-finetune/llama-3-8b/wikitext/$MODEL-RHT-FT-2/ \
         --dataset_name wikitext \
         --dataset_config_name wikitext-2-raw-v1 \
-        --block_size 512 \
-        --learning_rate 3e-6 \
+        --base_model meta-llama/Meta-Llama-3-8B \
+        --block_size 1024 \
+        --learning_rate 1e-6 \
         --num_train_epochs 3 \
         --per_device_train_batch_size 2 \
         --per_device_eval_batch_size 2 \
--- shell_scripts/run_quip_ppl.sh
+++ shell_scripts/run_quip_ppl.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
-python scripts/quip_llama_ppl.py \
-    --model_save_path /media/hdd1/lplr-q-models/llama-2-7b/lplr-ldlq-16B-factors-hessian-downdate \
-    --finetune_save_dir /media/hdd1/lplr-q-finetune/llama-2-7b/wikitext/lplr-ldlq-16B-factors-hessian-downdate-2 \
-    --output_path data/ppl-temp/llama-2-7b/lplr-ldlq-16B-factors-downdate-2 \
-    --device cuda:7 \
-    --base_model meta-llama/Llama-2-7b-hf \
-    --seed 0 \
-    --seqlen 4096 \
-    --datasets wikitext2
+i=200
+MODEL="lplr-ldlq-rank-256-4B-factors-downdate"
 
-for i in {200..2000..200}
+for i in {100..900..100}
 do
-    echo $i
-    python scripts/quip_llama_ppl.py \
-    --model_save_path /media/hdd1/lplr-q-models/llama-2-7b/lplr-ldlq-16B-factors-hessian-downdate \
-    --finetune_save_dir /media/hdd1/lplr-q-finetune/llama-2-7b/wikitext/lplr-ldlq-16B-factors-hessian-downdate-2/checkpoint-$i\
-    --output_path data/ppl-temp/llama-2-7b/lplr-ldlq-16B-factors-downdate-2-$i \
-    --device cuda:7 \
-    --base_model meta-llama/Llama-2-7b-hf \
+python scripts/quip_llama_ppl.py \
+    --model_save_path /media/hdd1/lplr-q-models/llama-3-8b/$MODEL \
+    --output_path data/ppl-temp/llama-3-8b/rht_ft_test_rank_256\
+    --device cuda:2 \
+    --base_model meta-llama/Meta-Llama-3-8B \
     --seed 0 \
-    --seqlen 4096 \
-    --datasets wikitext2
+    --seqlen 8192 \
+    --datasets wikitext2 \
+    --finetune_save_dir /media/hdd1/lplr-q-finetune/llama-3-8b/wikitext/$MODEL-RHT-FT-2/checkpoint-$i
 done
-
-
-# python scripts/quip_llama_ppl.py \
-#     --model_save_path /media/hdd1/lplr-q-models/llama-3-8b/lplr-ldlq-rank-256-4B-factors-downdate \
-#     --output_path data/ppl-temp/llama-3-8b/temp-test-256 \
-#     --device cuda:7 \
-#     --base_model meta-llama/Meta-Llama-3-8B \
-#     --seed 0 \
-#     --seqlen 8192
