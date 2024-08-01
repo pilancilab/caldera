cfb230a8e18fc4ed1c1a688e7fc7a7e4458648e1
Varun Srivastava <varunsrivastava.v@gmail.com>
2024-08-01 06:47:07 -0700

    feat(scripts): update evaluation and quantization scripts for Llama-2-13b
    - Set quantization parameters for better configuration management and removed un
    necessary random SVD calculation.
    - Changed the base model from Llama-2-7b to Llama-2-13b in both run_eval_ppl.sh
    and run_quantize_save_caldera.sh.
    - Updated model paths and output file locations to reflect the new model version
    - Enhanced CUDA device allocation by adding support for up to 8 devices.
    - Improved error handling with `set -euxo pipefail` in run_quantize_save_caldera
    .sh for robustness.

--- shell_scripts/run_eval_ppl.sh
+++ shell_scripts/run_eval_ppl.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-BASE_MODEL="meta-llama/Llama-2-7b-hf"
-CALDERA_MODEL_SAVE_DIR="YOUR DIRECTORY HERE"
-OUTPUT_FILENAME="YOUR FILE HERE"
-FINETUNE_CHECKPOINT_DIR="YOUR DIRECTORY HERE"
+HF_MODEL="meta-llama/Llama-2-13b-hf"
+MODEL_PATH="llama-2-13b/256R-4B-6144H"
+CALDERA_MODEL_SAVE_DIR="/media/hdd1/caldera/quantized/$MODEL_PATH"
+OUTPUT_FILENAME="/media/hdd1/caldera/$MODEL_PATH/ppl-eval.json"
+FINETUNE_CHECKPOINT_DIR="$CALDERA_MODEL_SAVE_DIR"
 
-python scripts/eval_ppl.py \
+CUDA_VISIBLE_DEVICES="0" python scripts/eval_ppl.py \
     --model_save_path $CALDERA_MODEL_SAVE_DIR \
     --output_path  $OUTPUT_FILENAME \
-    --device cuda:0 \
-    --base_model $BASE_MODEL \
+    --base_model $HF_MODEL \
     --seed 0 \
     --seqlen 4096 \
     --datasets wikitext2 c4 \
-    --finetune_save_dir $FINETUNE_CHECKPOINT_DIR
+    --finetune_save_dir $FINETUNE_CHECKPOINT_DIR \
+    --device cuda:0
+
+# --device cuda:0 cuda:1 cuda:2 cuda:3 cuda:4 cuda:5 cuda:6 cuda:7 \
--- shell_scripts/run_quantize_save_caldera.sh
+++ shell_scripts/run_quantize_save_caldera.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-RANK=64
-BASE_MODEL="meta-llama/Llama-2-7b-hf"
-CALDERA_MODEL_SAVE_DIR="YOUR DIRECTORY HERE"
-HESSIAN_SAVE_DIR="YOUR DIRECTORY HERE"
-TOKEN="YOUR HUGGINGFACE TOKEN HERE"
+
+set -euxo pipefail
+
+RANK=256
+FT_RANK=0
+LORA_BITS=4
+CALIBRATION_SIZE=6144
+
+MODEL_NAME="llama-2-13b"
+HF_MODEL="meta-llama/${MODEL_NAME^}-hf"
+
+BASE_PATH="/media/hdd1/caldera"
+CALDERA_MODEL_SAVE_DIR="$BASE_PATH/quantized/$MODEL_NAME/${RANK}R-${LORA_BITS}B-${CALIBRATION_SIZE}H"
+
+# HESSIAN_SAVE_DIR=$(huggingface-cli download relaxml/${MODEL_NAME^}-${CALIBRATION_SIZE})
+HESSIAN_SAVE_DIR="/media/hdd1/huggingface/hub/models--relaxml--Hessians-Llama-2-13b-6144/snapshots/1eb1423bd94e2fb664a80715efcda44ccde44e3e"
+
+TOKEN="$HF_TOKEN"
 
 QUANT_PARAMS="--Q_bits 2 \
     --compute_low_rank_factors true \
     --compute_quantized_component true \
-    --L_bits 4 \
-    --R_bits 4 \
+    --L_bits $LORA_BITS \
+    --R_bits $LORA_BITS \
     --lattice_quant_LR true \
     --rank $RANK \
     --activation_aware_LR true \
@@ -17,15 +30,15 @@ QUANT_PARAMS="--Q_bits 2 \
     --hadamard_transform true \
     --iters 15 \
     --lplr_iters 10 \
-    --rand_svd true \
+    --rand_svd false \
     --update_order LR Q \
     --Q_hessian_downdate true \
-    --ft_rank 64"
+    --ft_rank $FT_RANK"
 
 python scripts/quantize_save_llama.py \
     --hessian_save_path $HESSIAN_SAVE_DIR \
     --model_save_path $CALDERA_MODEL_SAVE_DIR \
-    --devices cuda:0 cuda:1 cuda:2 cuda:3 \
-    --base_model $BASE_MODEL \
+    --devices cuda:0 cuda:1 cuda:2 cuda:3 cuda:4 cuda:5 cuda:6 cuda:7 \
+    --base_model $HF_MODEL \
     --token $TOKEN \
-    $QUANT_PARAMS \
+    $QUANT_PARAMS
